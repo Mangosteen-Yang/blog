33ad688d23139e0a2af5b90abad12ef34fa5b81d
Mangosteen-Yang <yangshuo225@gmail.com>
2019-06-10 20:27:10 +1000

    google analytic

--- gridsome.config.js
+++ gridsome.config.js
@@ -25,6 +25,12 @@ module.exports = {
           }
         }
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-141755037-1'
+      }
     }
   ],
 
